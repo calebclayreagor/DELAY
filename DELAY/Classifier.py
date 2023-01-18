7c085665361d44c8671adf17e9f14db5ad032c50
Caleb Reagor <49416466+calebclayreagor@users.noreply.github.com>
2023-01-18 10:17:26 -0800

    Torch cat sum

--- DELAY/Classifier.py
+++ DELAY/Classifier.py
@@ -120,7 +120,7 @@ class Classifier(pl.LightningModule):
         for idx in range(len(self.valnames)):
             test_auprc[idx], test_auroc[idx] = self.val_auprc[idx].compute(), self.val_auroc[idx].compute()
             target_idx = torch.cat(self.val_auprc[idx].target)
-            input(target_idx)
+            input(target_idx.sum())
             self.log(f'_{self.valnames[idx]}auprc', test_auprc[idx], sync_dist = True, add_dataloader_idx = False)
             self.log(f'_{self.valnames[idx]}auroc', test_auroc[idx], sync_dist = True, add_dataloader_idx = False)
             self.log(f'_{self.valnames[idx]}_density', target_idx.sum()/target_idx.size(0), sync_dist = True, add_dataloader_idx = False)
