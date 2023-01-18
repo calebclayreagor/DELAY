6850d05a84df1ee45d199753dc817a249af5e90c
Caleb Reagor <49416466+calebclayreagor@users.noreply.github.com>
2023-01-18 10:14:53 -0800

    Torch cat

--- DELAY/Classifier.py
+++ DELAY/Classifier.py
@@ -119,7 +119,7 @@ class Classifier(pl.LightningModule):
         test_auroc = torch.zeros((len(self.valnames),), device = torch.cuda.current_device())
         for idx in range(len(self.valnames)):
             test_auprc[idx], test_auroc[idx] = self.val_auprc[idx].compute(), self.val_auroc[idx].compute()
-            target_idx = self.val_auprc[idx].target
+            target_idx = torch.cat(self.val_auprc[idx].target)
             input(target_idx)
             self.log(f'_{self.valnames[idx]}auprc', test_auprc[idx], sync_dist = True, add_dataloader_idx = False)
             self.log(f'_{self.valnames[idx]}auroc', test_auroc[idx], sync_dist = True, add_dataloader_idx = False)
