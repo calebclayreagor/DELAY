e04c87730d1cf9f671c5b72a76f4cb86e7fe9e0a
Caleb Reagor <49416466+calebclayreagor@users.noreply.github.com>
2023-06-06 10:16:06 -0400

    GCN check graph

--- Networks/GCN.py
+++ Networks/GCN.py
@@ -5,6 +5,7 @@ from torch_geometric.nn import Sequential
 from typing import List
 from typing import TypeVar
 import numpy as np
+import networkx as nx
 
 Self = TypeVar('Self', bound = 'GCN')
 
@@ -12,29 +13,38 @@ class GCN(nn.Module):
     """GCN classifier"""
 
     def __init__(self: Self,
+                 graph: np.array,
                  cfg: List[int],
                  in_dimensions: int,
                  ) -> Self:
         super(GCN, self).__init__()
+
+        input(graph)
+
+        G = nx.from_numpy_array(graph, create_using = nx.DiGraph)
+        n_conv = 0
+        for node in list(G.nodes()):
+            d = nx.shortest_path_length(G, node, target = 0, weight = None)
+            if d > n_conv: n_conv = d
+        input(n_conv)
+
         self.features = self.make_layers(cfg, in_dimensions)
-        self.classifier = nn.Linear(cfg[-1], 1)                     
+        self.edge_index = torch.tensor(np.array(np.where(graph > 0)), dtype = torch.long)
+        self.classifier = nn.Linear(cfg[-1], 1)                   
         # self._initialize_weights()
 
-    def forward(self: Self,
-                x: torch.Tensor,
-                # edge_index: torch.Tensor
-                ) -> torch.Tensor:
-        edge_index = torch.tensor([[0,  1,  1,  1,  1,  2,  2,  2,  3,  3,  3,  4,  4,  4,  5,  5, 
-                                    5,  5,  6,  6,  6,  6,  6,  7,  7,  7,  8,  8,  8,  9,  9,  9,
-                                    10, 10, 10, 11, 11, 11, 12, 12, 12, 13, 13, 13, 14, 14, 14, 15,
-                                    15, 15, 16, 16, 16, 17, 17, 17, 18, 18, 18, 18, 19, 20, 21, 21,
-                                    21, 22, 23, 23, 23, 24, 25, 26],
-                                   [1,  0,  2,  4,  6,  1,  3,  7,  2,  4, 24,  1,  3,  5,  4,  6,
-                                    17, 19,  1,  5,  7,  8, 10,  2,  6, 23,  6,  9, 16,  8, 10, 13,
-                                    6,  9, 11, 10, 12, 23, 11, 13, 21,  9, 12, 14, 13, 15, 20, 14,
-                                    16, 18,  8, 15, 17,  5, 16, 18, 15, 17, 25, 26,  5, 14, 12, 22,
-                                    23, 21, 7, 11, 21, 3, 18, 18]], 
-                                   dtype = torch.long, device = torch.cuda.current_device())
+    def forward(self: Self, x: torch.Tensor) -> torch.Tensor:
+        # edge_index = torch.tensor([[0,  1,  1,  1,  1,  2,  2,  2,  3,  3,  3,  4,  4,  4,  5,  5, 
+        #                             5,  5,  6,  6,  6,  6,  6,  7,  7,  7,  8,  8,  8,  9,  9,  9,
+        #                             10, 10, 10, 11, 11, 11, 12, 12, 12, 13, 13, 13, 14, 14, 14, 15,
+        #                             15, 15, 16, 16, 16, 17, 17, 17, 18, 18, 18, 18, 19, 20, 21, 21,
+        #                             21, 22, 23, 23, 23, 24, 25, 26],
+        #                            [1,  0,  2,  4,  6,  1,  3,  7,  2,  4, 24,  1,  3,  5,  4,  6,
+        #                             17, 19,  1,  5,  7,  8, 10,  2,  6, 23,  6,  9, 16,  8, 10, 13,
+        #                             6,  9, 11, 10, 12, 23, 11, 13, 21,  9, 12, 14, 13, 15, 20, 14,
+        #                             16, 18,  8, 15, 17,  5, 16, 18, 15, 17, 25, 26,  5, 14, 12, 22,
+        #                             23, 21, 7, 11, 21, 3, 18, 18]], 
+        #                            dtype = torch.long, device = torch.cuda.current_device())
         for i in range(x.size(0)):
             xi = x[i, ...]                      # [nchan, nbins, nbins]
             id = np.indices(xi.size())          # [3, nchan, nbins, nbins]
@@ -49,10 +59,10 @@ class GCN(nn.Module):
             xi = torch.tile(xi, (27, 1))        # [27, 4 * nchan * nbins * nbins]
             if i == 0:
                 x_batch = xi
-                edge_index_batch = edge_index
+                edge_index_batch = self.edge_index
             else:
                 x_batch = torch.cat((x_batch, xi), dim = 0)
-                edge_index_batch = torch.cat((edge_index_batch, (27 * i) + edge_index), dim = 1)
+                edge_index_batch = torch.cat((edge_index_batch, (27 * i) + self.edge_index), dim = 1)
         out = self.features(x_batch, edge_index_batch)
         out = out[::27, ...]                    # [batch_size, cfg[-1]]
         return self.classifier(out)
